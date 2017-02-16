266912e6d6ef95b9ae70adde40cf5e0c6d9b5456
Vladimir Nevinniy <vladimir@Mac-mini-Vladimir.local>
2017-02-16 21:40:39 +0200

    first commit add NSCopying and NSCoding add save archive to file

--- ILearnNSCoding/ViewController.swift
+++ ILearnNSCoding/ViewController.swift
@@ -8,7 +8,51 @@
 
 import UIKit
 
+class MyObject: NSObject, NSCoding, NSCopying {
+    var number = 0
+    var string = ""
+    var child: MyObject?
+    
+    override init() {
+        
+    }
+    
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(string, forKey: "stringKey")
+        aCoder.encode(number, forKey: "intKey")
+        if let myChild = child {
+            aCoder.encode(myChild, forKey: "childKey")
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        string = aDecoder.decodeObject(forKey: "stringKey") as! String
+        number = Int(aDecoder.decodeInt32(forKey: "intKey"))
+        child = aDecoder.decodeObject(forKey: "childKey") as? MyObject
+        
+    }
+    
+    func copy(with zone: NSZone? = nil) -> Any {
+        let copy = MyObject()
+        copy.number = number
+        copy.string = string
+        copy.child = child?.copy() as? MyObject
+        
+        return copy
+    }
+}
+
+
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var stringTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var stringChildTextField: UITextField!
+    @IBOutlet weak var numberChildTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +64,63 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //MARK: - Arhiver and Save data
+    func dataFilePath() -> String {
+        let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        
+        let documentDirectory = paths[0] as NSString
+        
+        return documentDirectory.appendingPathComponent("data.archive") as String
+    }
+    
+    
+    func save(object: MyObject)  {
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWith: data)
+        archiver.encode(object, forKey: "keyValueString")
+        archiver.finishEncoding()
+        
+        let path = dataFilePath()
+        print(path)
+        data.write(toFile: path, atomically: true)
+    }
+    
+    
+    @IBAction func onSave(_ sender: Any) {
+        
+        let object = MyObject()
+        
+        object.string = stringTextField.text!
+       
+        
+        if let number = Int(numberTextField.text!) {
+            object.number = number
+        }
+        
+        if stringChildTextField.text != "" {
+            let childObject = MyObject()
+            
+            childObject.string = stringChildTextField.text!
+            if let number = Int(numberChildTextField.text!) {
+                object.number = number
+            }
+            
+            childObject.child = childObject
+        }
+        
+        save(object: object)
+        
+        stringTextField.text = ""
+        numberTextField.text = ""
+        
+        stringChildTextField.text = ""
+        numberChildTextField.text = ""
+        
+        
+    }
+    
+    
 
 }
 
